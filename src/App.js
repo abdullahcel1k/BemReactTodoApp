ef2586730eed9199eba7064159b557e650437e9a
Abdullah Çelik <abdullahcelkk@gmail.com>
2022-05-13 20:17:18 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -7,26 +7,12 @@ import {
 } from "react-router-dom";
 import "./App.scss";
 import About from "./components/About";
-import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import TodoList from "./components/TodoList";
-import TodoModal from "./components/TodoModal";
 import "./utils/interceptor";
 
 function App() {
-  const [isShow, setShow] = useState(false);
-  const [todoList, setTodoList] = useState([]);
 
-  const getTodos = async () => {
-    axios
-      .get("Todo")
-      .then((res) => setTodoList(res.data));
-  };
-
-  useEffect(() => {
-    getTodos();
-  }, []);
 
   return (
     <Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import axios from 'axios';
+import Header from "./Header";
+import TodoList from "./TodoList";
+import TodoModal from "./TodoModal";
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+
+ const [isShow, setShow] = useState(false);
+ const [todoList, setTodoList] = useState([]);
+
+ const getTodos = async () => {
+  axios
+   .get("Todo")
+   .then((res) => setTodoList(res.data));
+ };
+
+ useEffect(() => {
+  getTodos();
+ }, []);
  return (
   <section className="container">
    <Header setShow={setShow} />
